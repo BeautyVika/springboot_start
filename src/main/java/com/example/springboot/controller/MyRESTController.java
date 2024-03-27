e11f7e4e3dfe52f0638099c94e0159880b4ee8b5
Петухова Виктория Александровна <viktoryia.petukhova@softclub.by>
2024-03-27 10:49:04 +0300

    Initial commit

---
+++ src/main/java/com/example/springboot/controller/MyRESTController.java
@@ -0,0 +1,24 @@
+package com.example.springboot.controller;
+
+import com.example.springboot.entity.Employee;
+import com.example.springboot.service.EmployeeService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("api/")
+public class MyRESTController {
+    @Autowired
+    private EmployeeService employeeService;
+
+    @GetMapping("/employees")
+    public List<Employee> showAllEmployees() {
+        List<Employee> employeeList = employeeService.getAllEmployees();
+        return employeeList;
+    }
+
+}
